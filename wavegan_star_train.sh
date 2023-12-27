8086cdd6e3e53c8933bb64743c0d0eb2c2011d1e
univaxx <wchhiaarid@yandex.ru>
2023-12-27 14:46:28 +0000

    w* training process modified

---
+++ wavegan_star_train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear && python wavegan_star_train.py train \
+    --exp_name "wavegan_star_model" \
+    --checkpoint_interval 10 \
+    --save_interval 100 \
+    --num_epochs 500 \
+    --start_epoch 0 \
+    --bs 32 \
+    --lr 0.0001 \
+    --b1 0.5 \
+    --b2 0.9 \
+    --device_id 2 \
+    --fold_idx 2 \
+    --checkpoint_path /gim/lv02/isviridov/code/gans/gan_ecg/gan_models/Pulse2Pulse/output/wavegan_star_model/cps/gan_p2p_epoch:300.pt
